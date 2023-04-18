6bd766793ee7dceb32c7b3de45311f920f3d37c8
kristenmazza <kbmazza@gmail.com>
2023-04-17 18:33:08 -0700

    Add classes / files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import init from "./dom-manipulation";
+import Task from "./task";
 
 init();
+console.log(new Task("Eat", "Food", "3/4/2023", "high"));
---
+++ src/project.js
@@ -0,0 +1,5 @@
+export default class Project {
+  constructor(title) {
+    this.title = title;
+  }
+}
